31d0d2a92260e691d142c07e1540e136d80ad413
Ubuntu <ubuntu@ip-172-31-56-234.ec2.internal>
2020-11-22 07:17:47 +0000

    validate some fields

--- db/migrate/20201122042129_add_custom_fields_to_users.rb
+++ db/migrate/20201122042129_add_custom_fields_to_users.rb
@@ -2,5 +2,6 @@ class AddCustomFieldsToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :name, :string
     add_column :users, :age, :integer
+    add_index :users, :name, unique: true
   end
 end
