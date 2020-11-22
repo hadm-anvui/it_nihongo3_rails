0c83f570775688feb8ca6179c8db4d488751a33f
Ubuntu <ubuntu@ip-172-31-56-234.ec2.internal>
2020-11-22 06:47:06 +0000

    profile, user and book image, some custom fields for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'static_pages/home'
+  root to: 'static_pages#home'
+  devise_for :users, :controllers => {:registrations => "registrations"}
+  resources :users
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
